a04734ce4d08abd55916ff3acb562add25bfeaa8
JamesWRC <38713144+JamesWRC@users.noreply.github.com>
2021-12-30 23:37:15 +1100

    init

--- functions/ota/[ota].js
+++ functions/ota/[ota].js
@@ -1,39 +1,15 @@
-import Head from 'next/head'
+import {Ota} from '../../src/pages/ota'
 
-import Post from '../components/post'
-
-import Ota from '../../pages/ota/ota';
-
-export async function onRequest(context) {
-    // Contents of context object
+export async function onRequestGet(context) {
     const {
-      request, // same as existing Worker API
-      env, // same as existing Worker API
-      params, // if filename includes [id] or [[path]]
-      waitUntil, // same as ctx.waitUntil in existing Worker API
-      next, // used for middleware or to fetch assets
-      data, // arbitrary space for passing data between middlewares
-    } = context;
-    var ota = context.params.ota
-    
+        request, // same as existing Worker API
+        env, // same as existing Worker API
+        params, // if filename includes [id] or [[path]]
+        waitUntil, // same as ctx.waitUntil in existing Worker API
+        next, // used for middleware or to fetch assets
+        data, // arbitrary space for passing data between middlewares
+      } = context;
 
+      var ota = params.ota
     return Ota(ota)
   }
-
-export default function IndexPage({ postList }) {
-  return (
-    <main>
-      <Head>
-        <title>Home page</title>
-      </Head>
-
-      <h1>List of posts</h1>
-
-      <section>
-        {postList.map((post) => (
-          <Post {...post} key={post.id} />
-        ))}
-      </section>
-    </main>
-  )
-}
---
+++ src/pages/ota.js
@@ -0,0 +1,11 @@
+export function Ota(ota){
+    return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Your OTA is: <code>{ota}</code>.
+            </p>
+          </header>
+        </div>
+      );
+}

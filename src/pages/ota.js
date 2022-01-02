91f3c9218d7c8277b78ce9c8f3b94fec266f6fff
JamesWRC <38713144+JamesWRC@users.noreply.github.com>
2022-01-03 00:01:14 +1100

    testing with mobile

--- src/pages/ota.js
+++ src/pages/ota.js
@@ -1,37 +1,77 @@
 import { useEffect } from "react";
 import { useSearchParams, useParams } from "react-router-dom";
-import { Helmet } from "react-helmet"
+import { Helmet } from "react-helmet" 
 import { ethers } from "ethers";
 import { Buffer } from "buffer"
+import QRCode from 'qrcode'
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const { v4: uuidv4 } = require('uuid');
+const METAMASK_BASE_LINK = 'https://metamask.app.link/dapp/';
+const WEB_SOCKET = 'prod-dauth-backend.us-east-1.elasticbeanstalk.com/ws'
 
 // var Eth = require('ethjs')
 // window.Eth = Eth
 export default function Ota(){
-    let { ota } = useParams();
+    let { otk } = useParams();
+    if(!otk){
+      otk = uuidv4()
+    }
+    const [searchParams, setSearchParams] = useSearchParams()
+    var userOnMobile = searchParams.get("mobile")
+
     useEffect(async () => {
-      // web3.personal.sign(web3.fromUtf8("Hello from Toptal!"), web3.eth.coinbase, console.log);
+
+      const client = new W3CWebSocket(`ws://${WEB_SOCKET}/${otk}`);
+
       console.log('Init web3')
-      // const web3 = new window.Web3('https://cloudflare-eth.com')
-      // const currentBlockNumber = await web3.eth.getBlockNumber()
-      // alert(currentBlockNumber)
+
+
       const ethereum = window.ethereum;
+      const qrCodeURL = `${METAMASK_BASE_LINK}/dauth.dev/otk/${otk}?mobile=true`
+      QRCode.toCanvas(document.getElementById('authQRCodeCanvas'), qrCodeURL, { errorCorrectionLevel: 'H' }, function (err, url) {
+        // console.log(url)
+      })
+      if (ethereum && userOnMobile === "t") {
 
-      if (ethereum) {
-
-        alert('MetaMask is installed!');
+        // alert('MetaMask is installed!');
         try{
           const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
           const account = accounts[0];
+          console.log(account)
           var from = account
           var provider = new ethers.providers.Web3Provider(ethereum);
           var signer = provider.getSigner();
-          // const msg = `0x${Buffer.from("exampleMessage", 'utf8').toString('hex')}`;
-          const msg = `0231456342`;
+          // var NONCE = uuidv4()
+          // var NONCE = 'dfc25738-cf54-4a48-bfbb-4c4b0d9aced7'
+          const exampleMessage = 'Test `personal_sign` message, with NONCE: ' +  String(otk);
+
+          const msg = `0x${Buffer.from(exampleMessage, 'utf8').toString('hex')}`;
+          // const msg = `0231456342`;
           const sign = await ethereum.request({
             method: 'personal_sign',
             params: [msg, from, 'Example password'],
           });
           alert(sign);
+          console.log(sign);
+
+          fetch('https://api.dauth.dev/walletAuth', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              OTK: otk,
+              fromWallet: account,
+              rawMessage: exampleMessage,
+              signedMessage: sign
+            })
+          }).then(response => response.json())
+          .then(data => console.log(data))
+        .catch(error =>{
+            console.log(error)
+        })
           // personalSignVerify.disabled = false;
 
         } catch (err) {
@@ -49,9 +89,12 @@ export default function Ota(){
             </Helmet>
           <header className="App-header">
             <p>
-              Your ota is {ota}
+            <canvas id="authQRCodeCanvas"></canvas>
+
+              Your ota is {otk}
                {/* Your ota is {props} */}
             </p>
+
             <a
               className="App-link"
               href="https://reactjs.org"

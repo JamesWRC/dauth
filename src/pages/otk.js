fcdcedb29b4f92e3ef2dcec24d8a84c05e10d562
JamesWRC <38713144+JamesWRC@users.noreply.github.com>
2022-01-05 21:04:22 +1100

    Update otk.js

--- src/pages/otk.js
+++ src/pages/otk.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useSearchParams, useParams } from "react-router-dom";
-import { Helmet } from "react-helmet" 
+import { Helmet } from "react-helmet"
 import { ethers } from "ethers";
 import { Buffer } from "buffer"
 import QRCode from 'qrcode'
@@ -12,130 +12,123 @@ const WEB_SOCKET = 'socket.dauth.dev'
 
 
 
-export default function Otk(){
+export default function Otk() {
 
-    const [searchParams, setSearchParams] = useSearchParams()
-    var userOnMobile = searchParams.get("m") // if users i using mobile to authenticate
-    var otk = searchParams.get("otk") // get the one time key
-    if(otk == undefined){
-      otk = uuidv4()
-    }
-    useEffect(async () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  var userOnMobile = searchParams.get("m") // if users i using mobile to authenticate
+  var otk = searchParams.get("otk") // get the one time key
+  if (otk == undefined) {
+    otk = uuidv4()
+  }
+  useEffect(async () => {
 
-      const ws = new W3CWebSocket(`wss://${WEB_SOCKET}/ws/${otk}`);
+    const ws = new W3CWebSocket(`wss://${WEB_SOCKET}/ws/${otk}`);
 
-      ws.onmessage = function (event) {
-        const json = JSON.parse(event.data);
-        console.log(`[message] Data received from server: ${json}`);
-        alert(json)
-        alert(JSON.stringify(json))
-        try {
-        if ((json.event = "data")) {
-        
-                console.log(json.data);
+    ws.onmessage = async function (event) {
+      const json = JSON.parse(event.data);
+      console.log(`Data received from server web socket: ${JSON.stringify(json)}`);
 
-              }
-            } catch (err) {
-              // whatever you wish to do with the err
-              console.log(err)
-            }
-        
-        };
-      console.log('Init web3')
+      // validate Auth
+      await verifyAuth(json.payload.JWT)
+
+    };
+    console.log('Init web3')
 
 
-      const ethereum = window.ethereum;
-      const qrCodeURL = `${METAMASK_BASE_LINK}/dauth.dev/auth/?m=t&otk=${otk}`
-      QRCode.toCanvas(document.getElementById('authQRCodeCanvas'), qrCodeURL, { errorCorrectionLevel: 'H' }, function (err, url) {
-      })
-      if (ethereum && userOnMobile === "t") {
+    const ethereum = window.ethereum;
+    const qrCodeURL = `${METAMASK_BASE_LINK}/dauth.dev/auth/?m=t&otk=${otk}`
+    QRCode.toCanvas(document.getElementById('authQRCodeCanvas'), qrCodeURL, { errorCorrectionLevel: 'H' }, function (err, url) {
+    })
+    if (ethereum && userOnMobile === "t") {
 
-        try{
-          const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-          const account = accounts[0];
-          console.log(account)
-          var from = account
-          var provider = new ethers.providers.Web3Provider(ethereum);
-          var signer = provider.getSigner();
+      try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        console.log(account)
+        var from = account
+        var provider = new ethers.providers.Web3Provider(ethereum);
+        var signer = provider.getSigner();
 
-          const exampleMessage = 'Test `personal_sign` message, with NONCE: ' +  String(otk);
+        const exampleMessage = 'Test `personal_sign` message, with NONCE: ' + String(otk);
 
-          const msg = `0x${Buffer.from(exampleMessage, 'utf8').toString('hex')}`;
+        const msg = `0x${Buffer.from(exampleMessage, 'utf8').toString('hex')}`;
 
-          const sign = await ethereum.request({
-            method: 'personal_sign',
-            params: [msg, from, 'Example password'],
-          });
+        const sign = await ethereum.request({
+          method: 'personal_sign',
+          params: [msg, from, 'Example password'],
+        });
 
-          fetch('https://api.dauth.dev/walletAuth', {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              OTK: otk,
-              fromWallet: account,
-              rawMessage: exampleMessage,
-              signedMessage: sign
-            })
-          }).then(response => response.json())
+        fetch('https://api.dauth.dev/walletAuth', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            OTK: otk,
+            fromWallet: account,
+            rawMessage: exampleMessage,
+            signedMessage: sign
+          })
+        }).then(response => response.json())
           .then(async (data) => {
             console.log(data)
-            // validate Auth
-            await verifyAuth(data.authResult.JWT)
-          }).catch(error =>{
+
+          }).catch(error => {
             console.log(error)
-        })
+          })
 
-        } catch (err) {
-          console.error(err);
-        }
-
+      } catch (err) {
+        console.error(err);
       }
-    })
 
-    return (
-        <div className="App">
-          <Helmet>
-          <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js" />
-          <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/eth-sig-util@3.0.1/dist/index.min.js" />
-            </Helmet>
-          <header className="App-header">
-            <p>
-            <canvas id="authQRCodeCanvas"></canvas>
+    }
+  })
 
-              Your ota is {otk}
-            </p>
+  return (
+    <div className="App">
+      <Helmet>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js" />
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/eth-sig-util@3.0.1/dist/index.min.js" />
+      </Helmet>
+      <header className="App-header">
+        <p>
+          <canvas id="authQRCodeCanvas"></canvas>
 
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      );
+          Your ota is {otk}
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 
-async function verifyAuth(jwt){
-  fetch('https://api.dauth.dev/verifyAuth', {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              JWT: jwt,
-            })
-          }).then(response => response.json())
-          .then(data => {
-            alert(data.tokenValid)
-          }).catch(error =>{
-            console.log(error)
-        })
+async function verifyAuth(jwt) {
+  var validJWT = await fetch('https://api.dauth.dev/verifyAuth', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      JWT: jwt,
+    })
+  }).then(response => response.json())
+    .then((data) => {
+      return data.tokenValid
+    }).catch(error => {
+
+      console.log(error)
+    })
+
+  return validJWT;
 }
